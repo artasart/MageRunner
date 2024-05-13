03abd2d7b1e65c1d5ba9869ef5e8167479e05055
cwj <134049423+artasart@users.noreply.github.com>
2024-05-13 18:55:03 +0900

    Set data error fixed

--- Assets/Scripts/Scene/Scene_Main.cs
+++ Assets/Scripts/Scene/Scene_Main.cs
@@ -36,8 +36,6 @@ public class Scene_Main : SceneLogic
 
 	protected override void OnDestroy()
 	{
-		GameManager.Backend.SetGameData();
-
 		JsonManager<GameData>.SaveData(LocalData.gameData, Define.JSON_GAMEDATA);
 		JsonManager<InvenData>.SaveData(LocalData.invenData, Define.JSON_INVENDATA);
 	}
@@ -49,8 +47,6 @@ public class Scene_Main : SceneLogic
 
 	public void SaveData()
 	{
-		GameManager.Backend.SetGameData();
-
 		LocalData.gameData.equipment = equipmentManager.equipments;
 
 		JsonManager<GameData>.SaveData(LocalData.gameData, Define.JSON_GAMEDATA);
@@ -154,14 +150,6 @@ public class Scene_Main : SceneLogic
 			GameManager.Backend.SetGameData();
 
 			PlayerPrefs.SetInt("isLogin", 1);
-		}
-	}
-
-	private void Update()
-	{
-		if(Input.GetKeyDown(KeyCode.Space))
-		{
-			GameManager.Backend.GetGameData();
 		}
 	}
 
